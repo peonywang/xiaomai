d259c361002a4b7e576dc725a6e0767ceb5c5434
wangguohua <wanggh@yilonghc.com>
2018-12-18 18:11:05 +0800

    fanpai

--- activity/fanpai/index.php
+++ activity/fanpai/index.php
@@ -30,19 +30,9 @@
             <p>我的奖品</p>
         </div>
     </div>
-
-<!--    "恭喜您获得iPhoneX!",-->
-<!--    "恭喜您获得",-->
-<!--    "恭喜您获得",-->
-<!--    "恭喜您获得",-->
-<!--    "恭喜您获得",-->
-<!--    "恭喜您获得",-->
-<!--    "恭喜您获得",-->
-<!--    "恭喜您获得",-->
-<!--    ""-->
     <?php
      $jiang = [
-         ['name' => "iPhoneX",        'src' => "images/iPhoneX.png"],
+//         ['name' => "iPhoneX",        'src' => "images/iPhoneX.png"],
          ['name' => "Huawei Mate 20", 'src' => "images/huawei.png"],
          ['name' => "199.99大红包",   'src' => "images/199hongbao.png"],
          ['name' => "30元优惠券",     'src' => "images/30yhq.png"],
@@ -50,7 +40,7 @@
          ['name' => "8.88元现金券",   'src' => "images/8xjq.png"],
          ['name' => "洗衣液",         'src' => "images/xiyiye.png"],
          ['name' => "餐巾纸",         'src' => "images/canjinzhi.png"],
-//         ['name' => "谢谢您的参与",   'src' => "images/xiexie.png"]
+         ['name' => "谢谢惠顾",   'src' => "images/xiexie.png"]
      ]
     ?>
     <div class="game">
@@ -72,7 +62,7 @@
             </ul>
         </div>
     </div>
-    <div class="change"><span>今日还剩<b>5</b>机会</span></div>
+    <div class="chance"><span>今日还剩<b><?php $chance = 1;echo $chance; ?></b>机会</span></div>
     <!--其他人中奖信息-->
     <div class="gun">
         <p class="p1"></p>
@@ -100,7 +90,7 @@
         <div class="nr">
             <div class="nrText">
                 <h3>▼我们的礼品：</h3>
-                <p>iPhoneX、Huawei Mate 20、199.99大红包、30元优惠券、20元优惠券、8.88元现金券、洗衣液、餐巾纸、谢谢参与</p>
+                <p>iPhoneX、Huawei Mate 20、199.99大红包、30元优惠券、20元优惠券、8.88元现金券、洗衣液、餐巾纸、谢谢惠顾</p>
 
                 <h3>▼如何获得抽奖机会：</h3>
                 <ol>
@@ -159,7 +149,7 @@
     <p class="btn"><img src="images/btn.png" alt="关闭" class="close" width="35" height="35"></p>
 </div>
 <!-- 中奖弹窗-->
-<div class="tan shuangTan winningTan" id="winningTan">
+<div class="tan shuangTan winningTan" id="winningTan" style="display: none">
     <h2>哇塞！恭喜您</h2>
     <div class="nrk" style="width:76%;margin:0 12%">
         <div class="nr">
@@ -175,193 +165,218 @@
 </div>
 <script>
     //游戏规则
-    //判断机会
-    var chance = 9;
-    var canChance = 5;
-    var useChance = 0;
-    var surplus = chance - canChance;
-    //设置几率及相应图片文字
-    var charsNum = [0,0,0,10,14,2,2,2,70];
-    //奖品数量
-    var jiangpin = charsNum.length - 1;
-    var jiangpinIndex = jiangpin - 1;//对应索引
-    var charsSum = 0;
-    for(var i = 0; i < jiangpin; i++){charsSum += charsNum[i];}
+
+
+    var youxi = $("#youxiqu");// 游戏区
+    var winningTan = $("#winningTan");//中奖弹窗
+    // 判断机会
+    var chance = <?php echo $chance; ?>;
+    // 奖品及概率
+    var charsTxt =[<?php foreach ($jiang as $k => $v){echo '"'.$v['name'].'",';} ?>];   // 奖品名称
+    //var imgUrl = [<?php foreach ($jiang as $k => $v){echo '"'.$v['src'].'",';} ?>];     // 奖品图片
+    var charsNum = [0,0,10,14,2,2,2,70]; //奖品概率
+    // 设置概率数组及随机数
     var chars = [];
-    //一等奖初次中奖概率
-    var firstJiangpin = 0.5;
-    //设置概率数组
-    function setChars(num){
-        chars[0]= charsSum*num/(1-num);
-        for(i = 1; i < jiangpin; i++){
-            chars[i] =  chars[i-1] + charsNum[i];
-        }
+    var suijiNum;
+    chars[0] =  charsNum[0];
+    for(i = 1; i < charsNum.length; i++){
+        chars[i] =  chars[i-1] + charsNum[i];
     }
-    //概率组
-    setChars(firstJiangpin);
-    // 对应索引的文字及图片
-    var charsTxt =[<?php foreach ($jiang as $k => $v){echo '"'.$v['name'].'",';} ?>];
-    var imgUrl = [<?php foreach ($jiang as $k => $v){echo '"'.$v['src'].'",';} ?>]
-    //初始化中奖次数
-    var caseNo = [];
-    for(i=0;i<jiangpin;i++){
-        caseNo.push(0);
-    };
+    // 获取随机数
+    function suiji() {
+        suijiNum = Math.round(Math.random()*(chars[charsNum.length-1]));
+        for(i=0;i<charsNum.length;i++){
+            if(suijiNum<chars[i]){
+                return i;
+                break;
+            }
+        };
+    }
+    // 奖品转动动画
+    var indexs = [0,1,2,5,8,7,6,3];//奖品li索引
 
-    //定义奖品等级变量
-    var jiangNo;
-    var kuaizhong = false;//是否快中奖变量
-    var kuaizhongNo;//快中奖索引
-    var zhongjiang = false;//是否中奖了
-    var failTan = $("#noWinningTan");
+    function donghua(fre,num,callback){ //fre 频率,中奖前转几次; num 中奖单元;
+        var time = 100;
+        var fre = fre?fre:2;
+        var i = 0;
+        function zhuanFn() {
+            $("li",youxi).eq(indexs[i]).addClass("act").siblings(".act").removeClass("act");
+            // 判断是否继续转
+            if(fre > 0){
+                if (i == 7){i = 0;fre--;time = time + 50;clearInterval(zhuan);zhuan = setInterval(zhuanFn,time);}
+                else{i++;}
+            }else{
+                i++;
+                console.log(fre,i);
+                if (i == (num + 1)){
+                    clearInterval(zhuan);
+                    setTimeout(callback,1000);
+                }
+            }
+        }
+        var zhuan = setInterval(zhuanFn,time);
+    }
 
-    //初始化
-    var fanpaiLi;//点击模块
-    var bgImgEven = "";//背景模块
-    var jiangpinEven = "";//奖品模块
-    var open = "";//点开状态
-    var openClass = "";//点开状态类名
-    var on = "";//牌可点击状态
-    var onClass = "";//牌可点击状态类名
-    var suiji;
-    var winningTan = $("#winningTan");
+
+    $(function () {
+        $("#begin").click(function () {
+            if(chance <= 0){
+                console.log("没机会了");
+                $(".chance b").text(0);
+            }else{
+                chance--;
+                $(".chance b").text(chance);
+                var jiangNo = suiji();
+                console.log(charsTxt[jiangNo]);
+                var num = 0;//需要跳几步
+                switch (jiangNo){ //suiji() 奖品索引
+                    case 0: num = 0;break;
+                    case 1: num = 1;break;
+                    case 2: num = 2;break;
+                    case 3: num = 7;break;
+                    case 4: num = 3;break;
+                    case 5: num = 6;break;
+                    case 6: num = 5;break;
+                    case 7: num = 4;break;
+                    default: break;
+                }
+                donghua(3,num,function () {
+                    if(jiangNo<7){
+                        $("#mywintext").html(charsTxt[jiangNo]);
+                        winningTan.show();
+                    }else{
+                        $("#mywintext").html(charsTxt[jiangNo]);
+                        winningTan.show();
+                    }
+                });
+            }
+        })
+    })
+//    var chance = 9;
+//    var canChance = 5;
+//    var useChance = 0;
+//    var surplus = chance - canChance;
+//    //设置几率及相应图片文字
+//    var charsNum = [0,0,0,10,14,2,2,2,70];
+//    //奖品数量
+//    var jiangpin = charsNum.length;
+//    var jiangpinIndex = jiangpin;//对应索引
+//    var charsSum = 0;
+////    console.log(jiangpinIndex);
+//    for(var i = 0; i < jiangpin; i++){charsSum += charsNum[i];}
+//    var chars = [];
+//    //一等奖初次中奖概率
+//    var firstJiangpin = 0.3;
+//    //设置概率数组
+//    function setChars(num){
+//        chars[0]= charsSum*num/(1-num);
+//        for(i = 1; i < jiangpin; i++){
+//            chars[i] =  chars[i-1] + charsNum[i];
+//            console.log(chars[i]);
+//        }
+//    }
+//    //概率组
+//    setChars(firstJiangpin);
+//    // 对应索引的文字及图片
+//    var charsTxt =[<?php //foreach ($jiang as $k => $v){echo '"'.$v['name'].'",';} ?>//];
+//    var imgUrl = [<?php //foreach ($jiang as $k => $v){echo '"'.$v['src'].'",';} ?>//]
+//    //初始化中奖次数
+//    var caseNo = [];
+//    for(i=0;i<jiangpin;i++){
+//        caseNo.push(0);
+//    };
+//
+//    //定义奖品等级变量
+//    var jiangNo;
+//    var kuaizhong = false;//是否快中奖变量
+//    var kuaizhongNo;//快中奖索引
+//    var zhongjiang = false;//是否中奖了
+//    var failTan = $("#noWinningTan");
+//
+//    //初始化
+//    var fanpaiLi;//点击模块
+//    var bgImgEven = "";//背景模块
+//    var jiangpinEven = "";//奖品模块
+//    var open = "";//点开状态
+//    var openClass = "";//点开状态类名
+//    var on = "";//牌可点击状态
+//    var onClass = "";//牌可点击状态类名
+//    var suiji;
+//    var winningTan = $("#winningTan");
 
     $(function () {
         //游戏开始按钮
-        $("#begin").click(function(){
-            $(this).attr("src","images/anxia.png");
-            $(".zmd,.zmd2").css("animation-duration","0.5s");
-            var beginHtml = '';
-            var youxiLiHtml = '<li class="on"> <div class="d1"> <img src="images/weixuanzhong.jpg"> </div> <div class="d2"> <img src=""> </div> </li>';
-            // var youxiLiHtml2 = '<li class="on mg0"> <div class="d1"> <img src="images/weixuanzhong.jpg"> </div> <div class="d2"> <img src=""> </div> </li>';
-            for(i = 0; i < 9; i++){
-                beginHtml = beginHtml + youxiLiHtml;
-            }
-            $("#youxiqu").delay(100).fadeOut(function(){
-                $(this).html(beginHtml);
-                //初始化
-                fanpaiLi = $("#youxiqu li.on");//点击模块
-                bgImgEven = ".d1 img";//背景模块
-                jiangpinEven = ".d2 img";//奖品模块
-                open = ".open";//点开状态
-                openClass = "open";//点开状态类名
-                on = ".on";//牌可点击状态
-                onClass = "on";//牌可点击状态类名
-                // var chanceD = $("#chance span");//机会显示框
-            }).delay(200).fadeIn(function(){
-                //记录已翻开的数据
-                function jiluNo(i){
-                    caseNo[i]++;
-                    if(caseNo[0]>0){
-                        setChars(0);
-                        chars[0] = -1;
-                    }
-                    if(caseNo[i]==2){
-                        kuaizhong = true;
-                        kuaizhongNo = i;
-                    }
-                }
-
-                //判断是否中奖
-                function panduan(){
-                    for(i=0;i<jiangpin;i++){
-                        if(caseNo[i]==3){
-                            winningTan.show();
-                            console.log('中奖了')
-                            caseNo[i]=0;
-                            zhongjiang = true;
-                            //document.location.href="zhong.php"//中奖跳转页面
-                        }
-                    }
-                }
-
-                //获取随机数
-                function suijiNum(){
-                    suiji = Math.round(Math.random()*(chars[jiangpinIndex]-1));
-                    for(i=0;i<jiangpin;i++){
-                        if(suiji<chars[i]){
-                            jiangNo = i;
-                            break;
-                        }
-                    };
-                }
-
-                //翻牌动画
-                function fanpai(bgImg,jiangpinImg,_thisjiangNo){
-                    bgImg.animate({width:0},200,"swing",function(){
-                        bgImg.parents(".d1").css("height",0).siblings(".d2").css("height","auto");
-                        // 记录已翻开的数据
-                        jiluNo(_thisjiangNo);
-                        console.log(_thisjiangNo,imgUrl[_thisjiangNo])
-                        jiangpinImg.attr("src",imgUrl[_thisjiangNo]).show().animate({width:"100%"},100,"swing",function(){
-                            panduan();
-                            fanpaiLi.addClass('on');
-                            if(useChance == canChance && !zhongjiang){
-                                for (var i = 0; i < surplus; i++) {
-                                    chance--;
-                                    console.log(i);
-                                    for(var j = 0; j < 9; j++){//9宫格
-                                        suijiNum();
-                                        if(!fanpaiLi.eq(j).is(open)){
-                                            var _thisOther = fanpaiLi.eq(j);
-                                            var bgImgOther = $(bgImgEven,_thisOther);
-
-                                            var jiangpinImgOther = $(jiangpinEven,_thisOther);
-                                            _thisOther.addClass(openClass);
-                                            jiangpinImgOther.attr("src",imgUrl[jiangNo]).css("width","100%").delay("500").fadeIn();
-                                            bgImgOther.delay("500").fadeOut();
-                                            failTan.delay("2000").fadeIn();
-
-                                            console.log("未中奖")
-                                            //document.location.href="zhong.php"//未中奖跳转页面
-
-                                            break;
-                                        }
-                                    }
-                                };
-                            }
-                        });
-                        // chanceD.html(chance);
-                    })
-                }
-
-                //点击翻牌
-                fanpaiLi.click(function() {
-                    var _this = $(this);
-                    var bgImg = $(bgImgEven,_this);//背景img
-                    var jiangpinImg = $(jiangpinEven,_this);//奖品img
-
-
-                    if(!_this.is(open)){
-                        if(_this.is('.on')){
-                            fanpaiLi.removeClass('on');
-                            chance--;
-                            useChance++;
-                            _this.addClass(openClass);
-
-
-                            //点击3次后仍未出现相同时
-                            if(useChance == 4 && !kuaizhong){
-                                var already = [];
-                                for(i=0;i<jiangpin;i++){
-                                    if(caseNo[i]>0){
-                                        already.push(i);
-                                    }
-                                }
-                                console.log("没一样的了:"+already);
-                                suiji = Math.round(Math.random()*(already.length-1));
-                                jiangNo = already[suiji];
-                                fanpai(bgImg,jiangpinImg,jiangNo);
-                            }else{
-                                //获得几率内的随机数
-                                suijiNum();
-                                fanpai(bgImg,jiangpinImg,jiangNo);
-                            }
-                        }
-                    }
-                });
-            });
-        });
+//        $("#begin").click(function(){
+//            $(this).attr("src","images/anxia.png");
+//            $(".zmd,.zmd2").css("animation-duration","0.5s");
+//            var beginHtml = '';
+//            var youxiLiHtml = '<li class="on"> <div class="d1"> <img src="images/weixuanzhong.jpg"> </div> <div class="d2"> <img src=""> </div> </li>';
+//            // var youxiLiHtml2 = '<li class="on mg0"> <div class="d1"> <img src="images/weixuanzhong.jpg"> </div> <div class="d2"> <img src=""> </div> </li>';
+//            for(i = 0; i < 9; i++){
+//                beginHtml = beginHtml + youxiLiHtml;
+//            }
+//            $("#youxiqu").delay(100).fadeOut(function(){
+//                $(this).html(beginHtml);
+//                //初始化
+//                fanpaiLi = $("#youxiqu li.on");//点击模块
+//                bgImgEven = ".d1 img";//背景模块
+//                jiangpinEven = ".d2 img";//奖品模块
+//                open = ".open";//点开状态
+//                openClass = "open";//点开状态类名
+//                on = ".on";//牌可点击状态
+//                onClass = "on";//牌可点击状态类名
+//                // var chanceD = $("#chance span");//机会显示框
+//            }).delay(200).fadeIn(function(){
+//                //记录已翻开的数据
+//                function jiluNo(i){
+//                    caseNo[i]++;
+//                    if(caseNo[0]>0){
+//                        setChars(0);
+//                        chars[0] = -1;
+//                    }
+//                    if(caseNo[i]==2){
+//                        kuaizhong = true;
+//                        kuaizhongNo = i;
+//                    }
+//                }
+//
+//                //判断是否中奖
+//                function panduan(){
+//                    for(i=0;i<jiangpin;i++){
+//                        if(caseNo[i]==3){
+//                            $("#mywintext").html(charsTxt[i]);
+//                            winningTan.show();
+//                            console.log('中奖了')
+//                            caseNo[i]=0;
+//                            zhongjiang = true;
+//                            //document.location.href="zhong.php"//中奖跳转页面
+//                        }
+//                    }
+//                }
+//
+//                }
+//
+//                //点击翻牌
+//                fanpaiLi.click(function() {
+//                    var _this = $(this);
+//                    var bgImg = $(bgImgEven,_this);//背景img
+//                    var jiangpinImg = $(jiangpinEven,_this);//奖品img
+//
+//
+//                    if(!_this.is(open)){
+//                        if(_this.is('.on')){
+//                            fanpaiLi.removeClass('on');
+//                            chance--;
+//                            useChance++;
+//                            _this.addClass(openClass);
+//
+//
+//
+//                        }
+//                    }
+//                });
+//            });
+//        });
         //关闭弹窗
         $(".close").click(function () {
             $(this).parents(".tan").hide();
