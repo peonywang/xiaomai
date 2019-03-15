c60bca12b061aa900592562db017ffcf69eb5d2d
peonywang <fhjl1989@163.com>
2019-03-15 21:41:03 +0800

    '大牌云集'

---
+++ activity/famous/index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>大牌云集</title>
+    <link rel="stylesheet" href="famous.css">
+</head>
+<body>
+    <div class="famous">
+        <div class="top flex">
+            <div class="d1"><img src="images/tit.png" alt="2019大牌折扣日"></div>
+            <div class="d2">
+                <a href="">申请退换</a>
+                <a href="">订单查询</a>
+            </div>
+            <div class="d3">
+                <a href=""><img src="images/share.png" alt="分享"></a>
+            </div>
+        </div>
+        <h1 class="banner"><img src="images/banner.jpg" alt="大牌云集"></h1>
+<!--        抢购中-->
+        <div class="buying">
+            <div class="d1 flex">
+                <span class="tit">团购大折扣 正在疯抢中</span>
+                <div class="pagination"></div>
+            </div>
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <a class="swiper-slide flex">
+                        <img src="images/pro1.jpg" alt="商品图">
+                        <div class="inf">
+                            <div class="name">篮球 男子 哈登场上款篮</div>
+                            <div class="pri flex">
+                                <div class="rmb">238</div>
+                                <div class="join">已有 <sapn class="red">5</sapn> 人参团 </div>
+                            </div>
+<!--                            倒计时-->
+                            <div class="count">
+                                距离本轮团购仅剩: <b>12</b>时  <b>34</b>分  <b>56</b>秒
+                            </div>
+                            <div class="go">
+                                <span>立即参团</span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+<!--        人气大牌.BEST  FOR YOU -->
+        <div class="best">
+            <h2>人气大牌.BEST  FOR YOU</h2>
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">slider1</div>
+                    <div class="swiper-slide">slider2</div>
+                    <div class="swiper-slide">slider3</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
